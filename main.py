a9bb7bf4452246b5be95042221815d018aca0eaa
YaVyatkin1 <yar2008x@gmail.com>
2023-07-03 18:20:02 +0500

    v2

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print()
+from telebot.async_telebot import AsyncTeleBot
+
+bot = AsyncTeleBot("6329139729:AAEgTQ7raiKCHY8aeOGi-xJEhzmPZJBmi68")
+
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, 'Приветствую тебя новый пользователь')
+
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    text_message = message.text
+    text_message = text_message.lower()
+    if 'дела' in text_message or 'настроение' in text_message:
+        await bot.reply_to(message, 'Хорошо, а у тебя?')
+    elif 'шутка' in text_message or 'анекдот' in text_message:
+        await bot.reply_to(message, 'Колобок повесился')
+    else:
+        await bot.reply_to(message, 'Извините я вас не понял?')
+
+import asyncio
+asyncio.run(bot.polling())
